28a8af9c837244c54255cd70ccbff236cf90f513
Robert Paul <github@robertpaul.dev>
2024-06-12 19:15:01 -0700

    add, rm, ls implemented

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,19 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    // add tags to a file
     Add {
         path: std::path::PathBuf,
         tags: Vec<String>,
     },
-    Ls {
-        tag: String,
-    },
+    // remove tags from a file
     Rm {
         path: std::path::PathBuf,
         tags: Vec<String>,
+    },
+    // get all tags for a file
+    Ls {
+        tag: String,
     },
 }
 
@@ -30,26 +33,43 @@ fn main() -> std::io::Result<()> {
     };
 
     let mut tag_map: HashMap<String, Vec<std::path::PathBuf>> = match contents.as_str() {
-        "" => match serde_json::from_str(&contents) {
-            Ok(hm) => hm,
+        "" => HashMap::new(),
+        _ => match serde_json::from_str(&contents) {
+            Ok(map) => map,
             _ => HashMap::new(),
         },
-        _ => HashMap::new(),
     };
 
     match &cli.command {
         Commands::Add { path, tags } => tags.into_iter().for_each(|t| match tag_map.get_mut(t) {
-            Some(v) => v.push(path.to_path_buf()),
+            Some(v) => {
+                if !v.contains(path) {
+                    v.push(path.to_path_buf())
+                }
+            }
             None => {
                 tag_map.insert(t.to_string(), vec![path.to_path_buf()]);
-                ()
             }
         }),
-        Commands::Ls { tag } => {}
-        Commands::Rm { path, tags } => {}
+        Commands::Rm { path, tags } => tags.into_iter().for_each(|t| match tag_map.get_mut(t) {
+            Some(v) => v.retain(|e| e != path),
+            None => (),
+        }),
+        Commands::Ls { tag } => match tag_map.get(tag) {
+            Some(v) => {
+                if v.is_empty() {
+                    println!("No items tagged with \"{:?}\"", tag)
+                } else {
+                    v.into_iter()
+                        .for_each(|e| println!("{}", e.to_str().expect("Invalid Path")))
+                }
+            }
+            None => println!("No items tagged with \"{:?}\"", tag),
+        },
     }
 
     let serialized = serde_json::to_string(&tag_map).unwrap();
     std::fs::write(".tags-rs.json", serialized)?;
+
     Ok(())
 }
