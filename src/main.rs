84abc39b82720af6b6d473d27e3f7bc231c530c4
Robert Paul <contact@robertpaul.dev>
2024-06-13 09:50:53 -0700

    Refactor to pull method logic out

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
@@ -10,12 +11,12 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     // add tags to a file
-    Add {
+    Tag {
         path: std::path::PathBuf,
         tags: Vec<String>,
     },
     // remove tags from a file
-    Rm {
+    Untag {
         path: std::path::PathBuf,
         tags: Vec<String>,
     },
@@ -25,11 +26,45 @@ enum Commands {
     },
 }
 
+fn add_tag(tag_map: &mut HashMap<String, Vec<PathBuf>>, path: &PathBuf, tags: &Vec<String>) {
+    tags.into_iter().for_each(|t| match tag_map.get_mut(t) {
+        Some(v) => {
+            if !v.contains(path) {
+                v.push(path.to_path_buf())
+            }
+        }
+        None => {
+            tag_map.insert(t.to_string(), vec![path.to_path_buf()]);
+        }
+    });
+}
+
+fn rm_tag(tag_map: &mut HashMap<String, Vec<PathBuf>>, path: &PathBuf, tags: &Vec<String>) {
+    tags.into_iter().for_each(|t| match tag_map.get_mut(t) {
+        Some(v) => v.retain(|e| e != path),
+        None => (),
+    })
+}
+
+fn ls(tag_map: &HashMap<String, Vec<PathBuf>>, tag: &String) {
+    match tag_map.get(tag) {
+        Some(v) => {
+            if v.is_empty() {
+                println!("No items tagged with \"{:?}\"", tag)
+            } else {
+                v.into_iter()
+                    .for_each(|e| println!("{}", e.to_str().expect("Invalid Path")))
+            }
+        }
+        None => println!("No items tagged with \"{:?}\"", tag),
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
     let mut tags_file = match dirs::home_dir() {
         Some(p) => p,
-        None => std::path::PathBuf::from(""),
+        None => PathBuf::from(""),
     };
     tags_file.push(".tags-rs.json");
     let contents = match std::fs::read_to_string(&tags_file) {
@@ -37,7 +72,7 @@ fn main() -> std::io::Result<()> {
         _ => String::from(""),
     };
 
-    let mut tag_map: HashMap<String, Vec<std::path::PathBuf>> = match contents.as_str() {
+    let mut tag_map: HashMap<String, Vec<PathBuf>> = match contents.as_str() {
         "" => HashMap::new(),
         _ => match serde_json::from_str(&contents) {
             Ok(map) => map,
@@ -46,31 +81,9 @@ fn main() -> std::io::Result<()> {
     };
 
     match &cli.command {
-        Commands::Add { path, tags } => tags.into_iter().for_each(|t| match tag_map.get_mut(t) {
-            Some(v) => {
-                if !v.contains(path) {
-                    v.push(path.to_path_buf())
-                }
-            }
-            None => {
-                tag_map.insert(t.to_string(), vec![path.to_path_buf()]);
-            }
-        }),
-        Commands::Rm { path, tags } => tags.into_iter().for_each(|t| match tag_map.get_mut(t) {
-            Some(v) => v.retain(|e| e != path),
-            None => (),
-        }),
-        Commands::Ls { tag } => match tag_map.get(tag) {
-            Some(v) => {
-                if v.is_empty() {
-                    println!("No items tagged with \"{:?}\"", tag)
-                } else {
-                    v.into_iter()
-                        .for_each(|e| println!("{}", e.to_str().expect("Invalid Path")))
-                }
-            }
-            None => println!("No items tagged with \"{:?}\"", tag),
-        },
+        Commands::Tag { path, tags } => add_tag(&mut tag_map, path, tags),
+        Commands::Untag { path, tags } => rm_tag(&mut tag_map, path, tags),
+        Commands::Ls { tag } => ls(&tag_map, tag),
     }
 
     let serialized = serde_json::to_string(&tag_map)?;
